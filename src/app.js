04d97fac11a13e940d7de468520397e9cf5ce3fa
seonmoon <gorbange@gmail.com>
2021-10-19 22:49:46 +0900

    add app.js(button event)

---
+++ src/app.js
@@ -0,0 +1,23 @@
+function onHome(){
+    const location = document.querySelector("#home").offsetTop;
+    window.scrollTo({top:location,behavior:"smooth"});
+}
+function onAutoRecord(){
+    const location = document.querySelector("#auto-record").offsetTop;
+    window.scrollTo({top:location,behavior:"smooth"});
+}
+function onMultiPlatform(){
+    const location = document.querySelector("#multi-platform").offsetTop;
+    window.scrollTo({top:location,behavior:"smooth"});
+}
+function onReport(){
+    const location = document.querySelector("#report").offsetTop;
+    window.scrollTo({top:location,behavior:"smooth"});
+}
+function onContact(){
+    const location = document.querySelector("#contact").offsetTop;
+    window.scrollTo({top:location,behavior:"smooth"});
+}
+function onSendEmain(){
+
+}
