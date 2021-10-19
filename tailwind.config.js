574d22b87b3d177cce67820246dc3074374f896d
kms <rkdaudtjd583@gmail.com>
2021-10-19 15:59:49 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // purge: {
+  //   enabled: true,
+  //   content: ["src/*.html"],
+  // },
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'daily-dev-tips': "#F89283"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
